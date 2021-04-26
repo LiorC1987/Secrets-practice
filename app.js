c329a1b6b458c74650e2462ab76d23d3b424ea36
LiorCaspi <liorcaspi@gmail.com>
2021-04-26 18:09:42 +0300

    logging in using hashing of MD5

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const express = require("express");
 const ejs =  require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
+
 const app = express();
 
 app.use(express.static("public"));
@@ -14,14 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = new  mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 })
-
-const secret = process.env.SECRET
-
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -40,7 +37,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if (err) {
@@ -56,7 +53,7 @@ app.post("/login", function (req, res) {
   const password = req.body.password
   User.findOne({email: username}, function(err, foundUser) {
     if (!err) {
-      if (foundUser.password === password) {
+      if (foundUser.password === md5(password)) {
         res.render("secrets");
     } else {
       console.log("incorrect password entered")
